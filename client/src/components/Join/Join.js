bda4e5169a4f9e99851ec497e9cff1c074f2cd09
Jacky Eng <jengworks10@gmail.com>
2020-10-07 11:44:08 -0400

    added a description in Join

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -10,11 +10,16 @@ const Join = () => {
 
   return (
     <div className="login-container d-flex flex-column">
-      <h1 className="header d-flex justify-content-center"> Join a room! </h1>
+      <h1 className="header"> Join a room! </h1>
+      <h2 className="desc"> 
+        Username & Room Code can be anything! <br></br>
+        Ex: "JohnSmithGuy" & "123" <br></br><br></br>
+        It may take a bit to connect to the server due to periodic server idling. Read more <a href='https://github.com/Jengworks/React-Web-Chat-App' target='__blank' >here</a>.
+      </h2>
       {/* username */}
-      <input className="d-flex justify-content-center mt-3" placeholder="Username" type="text" onChange={(event) => setName(event.target.value)} />      
+      <input className="mt-3" placeholder="Username" type="text" onChange={(event) => setName(event.target.value)} />      
       {/* room id */}
-      <input  className="d-flex justify-content-center mt-3" placeholder="Room Code" type="text" onChange={(event) => setRoom(event.target.value)} />
+      <input  className="mt-3" placeholder="Room Code" type="text" onChange={(event) => setRoom(event.target.value)} />
       {/* join button */}
       <Link className="button d-flex justify-content-center mt-3" onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
         <button type="submit"> Join </button>
